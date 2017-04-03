786cd1147358415aea4ff697d121429fc25a8fc6
blackmiaool <blackmiaool@gmail.com>
2017-04-03 12:23:59 +0800

    update some questions

---
+++ 43065565/index.js
@@ -0,0 +1,319 @@
+var csvData = `Associate,Manager
+Matt Herman,John Smith
+Jane Doe,John Smith
+Adam Brown,John Smith
+Susan Harris,John Smith
+Mike Jones,John Smith
+John Smith,Colin Krauss
+Colin Krauss,Ashley Carlin
+Ashley Carlin,Lia McDermott
+Evan Park,Lia McDermott
+Lauren Werner,Evan Park
+Shane Waterson,Evan Park
+Emma Smith,Evan Park
+Mike Gregory,Evan Park
+Jose Biggleman,Evan Park
+Michelle Spektor,Evan Park
+Juan Branch,Evan Park
+John Orbase,Evan Park
+Matt McCloud,Evan Park
+Kelsey Carsen,Evan Park
+Kelli Krazwinski,Colin Krauss
+Stephanie Goldstien,Colin Krauss
+Ryan Woolwine,Colin Krauss
+Kyle Bohm,Colin Krauss
+Sydney Yellen,Colin Krauss
+Shankar Murjhree,Colin Krauss
+Wayne Ellington,Colin Krauss
+Dwight Folds,Colin Krauss
+Ellen McGlynn,Colin Krauss
+Nicolas Smith,Colin Krauss
+Molly Ercole,Colin Krauss
+Scott Hane,Colin Krauss
+Regina McMahon,Colin Krauss
+Skip Holden,Colin Krauss
+Kadeem McPherson,Colin Krauss
+Ray Ortiz,Colin Krauss
+Janet Barnes,Colin Krauss
+Holly Gold,Colin Krauss
+Lance Martinez,Ashley Carlin
+Mike Lubow,Ashley Carlin
+Jordan Belsin,Ashley Carlin
+Tom Strithers,Ashley Carlin
+Jamie Raleigh,Ellen McGlynn
+Joseph Bowman,Ellen McGlynn
+Kylie Branch,Ellen McGlynn
+Lars Randall,Ellen McGlynn
+Carlos Barndt,Lia McDermott
+Leo Hastings,Lia McDermott
+Jaime Kellemen,Lia McDermott
+Harvey Klien,Lia McDermott
+Lia McDermott,Lia McDermott`;
+
+
+var data = d3.csvParse(csvData);
+
+var height = document.getElementById("tree-container").offsetHeight;
+var width = document.getElementById("tree-container").offsetWidth;
+var avatarRadius = 20;
+var translateOffset = 25;
+var radius = d3.min([height, width]) / 2;
+var cluster = d3.cluster()
+    .size([360, radius / 1.33])
+    // .separation(function(a,b){return (a.parent == b.parent ? 1:2)/a.depth;});
+
+var svg = d3.select("#tree-container").append("svg")
+    .attr("width", radius * 2)
+    .attr("height", radius * 2)
+    .attr("id", "tree-container-svg")
+    .append("g")
+    .attr("transform", "translate(" + radius + "," + height / 2 + ")");
+
+//Clip path needed for cicrular SVG avatars
+var defs = svg.append('defs');
+var clipPath = defs.append('clipPath')
+    .attr('id', 'clip-circle')
+    .append('circle')
+    .attr('r', avatarRadius - 2.5);
+
+function project(x, y) {
+        var angle = (x - 90) / 180 * Math.PI,
+            radius = y;
+        return [radius * Math.cos(angle), radius * Math.sin(angle)];
+}
+var diagonal = function (d) {
+    return "M" + project(d.x, d.y) + "C" + project(d.x, (d.y + d.parent.y) / 2) +
+        " " + project(d.parent.x, (d.y + d.parent.y) / 2) +
+        " " + project(d.parent.x, d.parent.y);
+}
+d3.selection.prototype.moveToFront = function () {
+    return this.each(function () {
+        this.parentNode.appendChild(this);
+    });
+};
+
+d3.selection.prototype.moveToBack = function () {
+    return this.each(function () {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
+};
+
+//http://www.d3noob.org/2014/01/tree-diagrams-in-d3js_11.html
+function treeify(list, callback) {
+
+    var dataMap = list.reduce(function (map, node) {
+        map[node.Associate] = node;
+        return map;
+    }, {});
+
+    var treeData = [];
+    list.forEach(function (node) {
+        //Assuming the highest node is the last in the csv file
+        if (node.Manager === node.Associate) {
+            node.Manager = "Board of Directors"
+            callback(node);
+        }
+        // add to parent
+        var parent = dataMap[node.Manager];
+
+        if (parent) {
+            // create child array if it doesn't exist
+            (parent.children || (parent.children = []))
+            // add node to child array
+            .push(node);
+        } else {
+            // parent is null or missing
+            treeData.push(node);
+        }
+    });
+};
+
+function findItem(root, name, callback) {
+    var stack = [];
+    stack.push(root);
+    while (stack.length !== 0) {
+        var element = stack.pop();
+        if (element.Associate === name) {
+            callback(element);
+            return;
+        }
+        //The up, uncompressed case
+        else if (element.children !== undefined && element.children.length > 0) {
+            for (var i = 0; i < element.children.length; i++) {
+                stack.push(element.children[i]);
+            }
+        }
+        //The down (compressed) case
+        else if (element._children !== undefined && element._children.length > 0) {
+            for (var j = 0; j < element._children.length; j++) {
+                stack.push(element._children[j]);
+            }
+        }
+    }
+}
+
+function defaultPlot(root, elem) {
+    findItem(root, elem, function (d) {
+        //Showing 1 up and below
+        findItem(root, d.Manager, function (x) {
+            (x.children) ? x.children.forEach(collapse): x.children = x._children;
+            drawIt(x, root);
+        })
+    })
+}
+
+function collapse(d) {
+    if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = undefined;
+    }
+}
+
+//For the buggy transition interruption with many nodes
+function showAllCurrentPathsAndNodes() {
+    d3.selectAll(".link").style("opacity", 1);
+    d3.selectAll(".node").style("opacity", 1);
+}
+
+// Toggle children on click.
+function clickedNode(d, root) {
+    //Accounting for the transition bug on the delay
+    showAllCurrentPathsAndNodes();
+
+    if (d.children) {
+        d._children = d.children;
+        d.children = undefined;
+        drawIt(root)
+    } else {
+        d.children = d._children;
+        d._children = undefined;
+        drawIt(root)
+    }
+}
+
+//http://bl.ocks.org/syntagmatic/4092944
+function drawIt(root) {
+    //var nodes = cluster.nodes(root);
+    var nodes = d3.hierarchy(root);
+
+    var maxDepth = d3.max(nodes, function (d) {
+        return d.depth
+    });
+
+
+
+    cluster(nodes);
+    //    var nodes = root.descendants();
+    var links = nodes.descendants().slice(1)
+        //    var link = svg.selectAll("path.link").data(cluster.links(nodes));    
+    var link = svg.selectAll("path.link").data(links);
+    //This nodes callback is SUPER important for the update pattern
+    var node = svg.selectAll("g.node").data(nodes.descendants());
+
+    link.transition().duration(1000)
+
+
+    d3.selectAll(".node-cicle").classed("highlight", false);
+
+    showAllCurrentPathsAndNodes();
+    
+
+    link.enter().append("path")
+        .attr("class", "link")
+        .attr("d", diagonal)
+        .attr("", function (d) {
+            d3.select(this).moveToBack();
+        })
+        .style("opacity", 0)
+        .transition()
+        .duration(300)
+        .delay(function (d, i) {
+            return 28 * i;
+        }).style("opacity", 1);
+
+    node.transition().duration(800).attr("transform", function (d) {
+        return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+    });
+
+    var g = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+        })
+        .style("opacity", 0)
+        .style("cursor", function (d) {
+            return ((d._children || d.children) && d.Manager !== "Board of Directors") ? "pointer" : "not-allowed";
+        })
+        .on("mouseover", function () {
+            d3.select(this).moveToFront();
+        })
+
+    //Cant trust the enter append here, reassign the event listener for all nodes each draw
+    d3.selectAll(".node")
+        .on("click", function (d) {
+            return ((d._children || d.children) && d.Manager !== "Board of Directors") ? clickedNode(d, root) : "";
+        });
+
+
+    g.transition().duration(300)
+        .delay(function (d, i) {
+            return 28 * i;
+        })
+        .style("opacity", 1);
+
+    g.append("circle")
+        .attr("r", avatarRadius)
+        .attr("class", "circle-marker")
+        .style("stroke", function (d) {
+            d = d.data;
+            return ((d._children || d.children) && d.Manager !== "Board of Directors") ? "steelblue" : "gray";
+        })
+        .style("fill", function (d) {
+            d = d.data;
+            return ((d._children || d.children) && d.Manager !== "Board of Directors") ? "steelblue" : "#fff";
+        });
+
+    g.append("svg:image")
+        .attr("class", "node-avatar")
+        .attr("xlink:href", "http://safariuganda.com/wp-content/uploads/2014/12/480px-Facebook-default-no-profile-pic.jpg")
+        .attr("height", avatarRadius * 2)
+        .attr("width", avatarRadius * 2)
+        .attr("x", "-" + avatarRadius)
+        .attr("y", "-" + avatarRadius)
+        .attr('clip-path', 'url(#clip-circle)');
+
+    //Might want to tween this?
+    d3.selectAll(".node-avatar")
+        .attr("transform", function (d) {
+            return "rotate(" + (-1 * (d.x - 90)) + ")";
+        });
+
+    g.append("text")
+        .attr("dy", ".31em")
+        .attr("class", "label-text")
+        .text(function (d) {
+            return d.data.Associate;
+        })
+
+    //search all labels to ensure they are right side up (cant rely on the enter append here)
+    d3.selectAll(".label-text")
+        .attr("text-anchor", function (d) {
+            return d.x < 180 ? "start" : "end";
+        })
+        .attr("transform", function (d) {
+            return d.x < 180 ? "translate(" + translateOffset + ")" : "rotate(180)translate(-" + translateOffset + ")";
+        })
+
+    link.exit().transition().duration(0).style("opacity", 0).remove();
+    node.exit().transition().duration(0).style("opactiy", 0).remove();
+
+}
+
+treeify(data, function (treeReturn) {
+    var root = treeReturn;
+    defaultPlot(root, root.children[0].Associate)
+});
---
+++ 43113822/index.js
@@ -0,0 +1,385 @@
+const dataset = [{
+    "country": "Greece",
+    "Vodafone": 57,
+    "Wind": 12,
+    "Cosmote": 20
+}, {
+    "country": "Italy",
+    "Vodafone": 40,
+    "Wind": 24,
+    "Cosmote": 35
+}, {
+    "country": "France",
+    "Vodafone": 22,
+    "Wind": 9,
+    "Cosmote": 9
+}];
+
+const margin = {
+    top: 5, //top: 20,
+    right: 20,
+    bottom: 40,
+    left: 50
+};
+
+
+const parentNode = d3.select('#chart-id').node(),
+    parent = '#chart-id';
+
+var containerwidth = parentNode.getBoundingClientRect().width,
+    containerheight = parentNode.getBoundingClientRect().height,
+    width = containerwidth - margin.left - margin.right,
+    height = containerheight - margin.top - margin.bottom;
+
+
+var svg = d3.select(parent)
+    .append('svg')
+    .attr('width', containerwidth)
+    .attr('height', containerheight)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+/* Tooltip */
+
+var tooltip = d3.select(parent)
+    .append('div')
+    .attr('class', 'd3-tooltip hidden');
+
+var tooltipWidth, lastDate;
+var mousemoveFunc = function (d, i) {
+    var mouse = d3.mouse(svg.node()).map(function (d) {
+        console.log("d",d);
+        return parseInt(d);
+    });
+    var left = Math.min(containerwidth, mouse[0] + margin.left),
+        top = Math.min(containerheight, mouse[1] + margin.top);
+
+    tooltipWidth = d3.select(parent + ' .d3-tooltip').node().getBoundingClientRect().width;
+
+    //console.log(dataset[i]); // i is passed from the .d3-group
+
+    var ds = [];
+    for (var k in dataset[i]) {
+        ds.push(k + ": " + "<strong>" + dataset[i][k] + "</strong>"); // d[i] = value  // or dataset[i][k]
+    }
+    //        console.log(ds);
+    nds = ds.slice(1, length - 1);
+
+    tooltip
+        .html(nds.join("<br/>"))
+        .classed('hidden', false);
+
+    tooltip.attr('style', 'left:' + (left) + 'px;top:' + (top - margin.top) + 'px;transform:translate(-50%,-100%);') // centered tooltip
+
+};
+
+var mouseoutFunc = function (d, i) {
+    tooltip.classed('hidden', true);
+};
+
+/* Rectangle to receive user events */
+
+var clipPath = svg.append("defs")
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0);
+
+var view = svg
+    .append("rect")
+    .attr("class", "view")
+    .attr("x", 0)
+    .attr("y", 0);
+
+
+/* Zoom function */
+
+var zoom = d3.zoom()
+    .scaleExtent([1, 2])
+    .translateExtent([
+            [0, 0],
+            [width, height]
+        ])
+    .extent([
+            [0, 0],
+            [width, height]
+        ])
+    .on("zoom", redraw);
+
+svg.call(zoom); // Call zoom function
+
+
+var transform = d3.zoomTransform(svg.node()); // Get svg transform properties to rescale Axes/redraw zoomed lines
+
+var x = d3.scaleBand().padding(0.1), // No domains yet!
+    y = d3.scaleLinear();
+
+var xz = x,
+    xy = y; // Used to store zoom scales
+
+var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+var colorScale20 = d3.scaleOrdinal() // My color scales
+    .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+    .range(["#21569a", "#2d74d0", "#4196fe", "#80b8fd", "#7a2c99", "#ab2ecd", "#cf44fc", "#d883fc", "#a91c50", "#dd2170", "#ff3e9e", "#ff81be", "#b8370d", "#fa5802", "#ff753f", "#ff9f7f", "#9a9121", "#c8b331", "#fee041", "#f9df82"]);
+
+var colorScale10 = d3.scaleOrdinal()
+    .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    .range(["#2d74d0", "#80b8fd", "#ab2ecd", "#d883fc", "#dd2170", "#ff81be", "#fa5802", "#ff9f7f", "#c8b331", "#f9df82"]);
+
+var colorScale5 = d3.scaleOrdinal()
+    .domain([1, 2, 3, 4, 5])
+    .range(["#2d74d0", "#ab2ecd", "#dd2170", "#fa5802", "#c8b331"]);
+
+var keys = d3.keys(dataset[0]).slice(1);
+//console.log(keys[2]);
+
+// keys = d3.keys(dataset[0]).slice(2); // here is the key to remove stuff
+
+
+dataset.forEach(function (d, i) {
+    d.total = d3.sum(d3.values(d).slice(1));
+});
+//console.log(d3.max(total));
+//console.log(dataset);
+
+
+var fKeys = keys.slice();
+
+var xAxis = d3.axisBottom(x).tickSizeInner(2).tickSizeOuter(0);
+var yAxis = d3.axisLeft(y).ticks(5).tickSizeInner(-width).tickSizeOuter(0);
+var xAxisGroup = svg.append('g').attr('class', 'x-axis-group').append('g').attr('class', 'x d3-axis').attr('transform', 'translate(0,' + height + ')');
+var yAxisGroup = svg.append('g').attr('class', 'y d3-axis').attr('transform', 'translate(0,0)');
+
+var gg = svg.append('g').attr("class", "d3-group-wrap"); // keep d3 groups organized and used for correct clipping
+
+var yAxisLabel = svg.append('text')
+    .attr('class', 'd3-chart-label label-y-axis')
+    .text('Y Axis Units')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', -margin.left / 2)
+    .attr('dy', -5)
+    .attr('text-anchor', 'middle');
+
+//console.log(keys);
+
+var legendItem = d3.select(".legend") // Create legend (not needed inside redraw function, renders only once)
+    .selectAll("li")
+    .data(keys)
+    .enter()
+    .append("li")
+    .on('click', function (keyToToggle) {
+        // Go through both keys and fKeys to find out proper
+        // position to insert keyToToggle if it is to be inserted
+        var i, j;
+        for (i = 0, j = 0; i < keys.length; i++) {
+            // If we hit the end of fKeys, keyToToggle
+            // should be last
+            if (j >= fKeys.length) {
+                fKeys.push(keyToToggle);
+                break;
+            }
+            // if we found keyToToggle in fKeys - remove it
+            if (fKeys[j] == keyToToggle) {
+                // remove it
+                fKeys.splice(j, 1);
+                break;
+            }
+
+            // we found keyToToggle in the original collection
+            // AND it was not found at fKeys[j]. It means
+            // it should be inserted to fKeys at position "j"
+            if (keys[i] == keyToToggle) {
+                // add it
+                fKeys.splice(j, 0, keyToToggle);
+                break;
+            }
+
+            if (keys[i] == fKeys[j])
+                j++;
+        }
+
+        redraw();
+    });
+
+legendItem
+    .append("span")
+    .attr("class", "color-square")
+    .style("background", function (d) {
+        return colorScale5(d);
+    });
+
+legendItem
+    .append("span")
+    .text(function (d) {
+        return (d)
+    });
+
+var tickArr = [];
+redraw();
+
+function redraw() {
+
+    containerwidth = parentNode.getBoundingClientRect().width;
+    containerheight = parentNode.getBoundingClientRect().height;
+    width = containerwidth - margin.left - margin.right;
+    height = containerheight - margin.top - margin.bottom;
+
+    // Used for resize
+    d3.select(parent + ' svg, ' + parent + ' svg g').attr('width', containerwidth).attr('height', containerheight);
+
+    zoom
+        .translateExtent([
+                [0, 0],
+                [width, height]
+            ])
+        .extent([
+                [0, 0],
+                [width, height]
+            ])
+
+    clipPath
+        .attr("width", width)
+        .attr("height", containerheight);
+
+    view
+        .attr("width", width)
+        .attr("height", height);
+
+    //tooltip.classed('hidden', true);
+
+
+    transform = d3.zoomTransform(svg.node());
+
+    var stackedData = d3.stack().keys(fKeys)(dataset);
+    var autoScaleY = true; // scale Y according to selected data or always use original range
+    var maxDataY;
+    if (autoScaleY && stackedData.length > 0) {
+        maxDataY = (1.2 * d3.max(stackedData, function (d) {
+            return d3.max(d, function (innerD) {
+                return innerD[1];
+            });
+        }));
+    } else {
+        maxDataY = (1.2 * d3.max(dataset, function (d) {
+            return d.total;
+        }));
+    }
+    y.domain([0, maxDataY]).rangeRound([height, 0]);
+
+
+    x.domain(dataset.map(function (d) {
+        return d.country
+    })).rangeRound([0, width]);
+
+
+    xz = x.rangeRound([0, width * transform.k], .1 * transform.k);
+    yz = transform.rescaleY(y);
+
+
+    //xAxisLabel.attr('x', width).attr('y', containerheight-10)
+
+    xAxisGroup
+        .attr("transform", "translate(" + transform.x + "," + (height) + ")")
+        .call(xAxis.scale(xz));
+
+    //xAxisGroup.selectAll(".tick text")
+    //.call(wrap, xz.bandwidth());
+
+
+    // Auto rotate ticks
+
+    xTicks = d3.selectAll(".x.d3-axis .tick text");
+
+    xTicks.each(function () {
+        text = d3.select(this);
+        textWidth = text.node().getComputedTextLength();
+        tickArr.push(textWidth);
+
+    });
+    textWidthMax = d3.max(tickArr);
+
+    if (textWidthMax > xz.bandwidth()) {
+        xTicks
+            .style("text-anchor", "middle")
+            //.attr("x", textWidthMax/2)
+            .attr("x", 0)
+            .attr("y", 0)
+            //.attr("dy", "1.5em")
+            .attr("transform", "translate(0,15)rotate(-30)");
+    } else {
+        xTicks
+        //.style("text-anchor", "middle")
+            .attr("x", "0")
+            .attr("y", 5)
+            .attr("dy", "0.75em")
+            .attr("transform", "rotate(0)");
+    }
+
+
+    yAxisLabel.attr('x', -containerheight / 2);
+    yAxis.tickSizeInner(-width);
+    yAxisGroup.call(yAxis.scale(yz))
+        .selectAll("text")
+        .attr("dx", "-0.5em");
+
+    // update selection
+
+    stackedBars = gg
+        .selectAll(".d3-group")
+        .data(stackedData, function (__data__, i, group) {
+            return __data__.key;
+        });
+
+    // exit the whole group
+    stackedBars
+        .exit().remove();
+
+    // enter selection
+    stackedBarsEnter = stackedBars
+        .enter()
+        .append("g")
+        .attr("fill", function (d) {
+            return colorScale5(d.key);
+        })
+        .attr("class", function (d, i) {
+            return "d3-group d3-group-" + i;
+        });
+
+    // add path on enter
+    bars = stackedBarsEnter
+        .selectAll('rect')
+        .data(function (d) {
+            return d;
+        })
+        .enter().append("rect")
+        .attr('class', 'd3-rect');
+
+
+    // update + enter
+    stackedBars = stackedBars.merge(stackedBarsEnter);
+
+    stackedBars.selectAll('.d3-rect')
+        .data(function (d) {
+            return d;
+        })
+        .attr("x", function (d) {
+            return xz(d.data.country);
+        })
+        .attr("y", function (d) {
+            return yz(d[1]);
+        })
+        .attr("height", function (d) {
+            return yz(d[0]) - yz(d[1]);
+        })
+        .attr("width", xz.bandwidth())
+        .attr("transform", "translate(" + transform.x + ",0)")
+        .on('mouseenter mousemove', mousemoveFunc)
+        .on('mouseout', mouseoutFunc);
+
+
+}
+$(window).on('resize', function () {
+    svg.call(zoom.transform, d3.zoomIdentity);
+});
